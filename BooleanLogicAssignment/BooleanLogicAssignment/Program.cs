8b08526e702a3c0de97f9db100b8f59e0cc0cdf7
Andy Taylor <andrew.taylor_22@yahoo.com>
2024-02-02 11:55:04 +0000

    Boolean Logic Assignment

---
+++ BooleanLogicAssignment/BooleanLogicAssignment/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooleanLogicAssignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Car Insurance Application\n");
+            
+            Console.WriteLine("What is your age?");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("\nHave you ever had a DUI? (Yes or No)");
+            string answer = Console.ReadLine();
+            bool DUI = answer == "yes";  
+
+            Console.WriteLine("\nHow many speeding tickets do you have?");
+            int tickets = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("\nQualified");
+            bool qualified = (age > 15) && !DUI && (tickets <= 3); //Checks if answers meet all critieria using boolean AND orerator
+            Console.WriteLine(qualified);
+        }
+    }
+}
