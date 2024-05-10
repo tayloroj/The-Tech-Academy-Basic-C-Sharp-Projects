96e9f359abfdcef3b5f4a2164515c78f2f5dcdeb
Andy Taylor <andrew.taylor_22@yahoo.com>
2024-05-10 11:44:45 +0100

    TwentyOne initial build

---
+++ TwentyOne/Card.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwentyOne
+{
+    public class Card
+    {
+        //Constuctor - values assigned to an object upon creation
+        public Card() 
+        {
+            Suit = "Spades";
+            Face = "Two";
+        }
+        
+        //In the line below the data type is "string", "Suit" is the name of the property, "get and set" are the only things to do with an object property.
+        //The card class has a property of data type "string" called "Suit" and you can "get" this property or "set" this property
+        public string Suit { get; set; }
+        public string Face { get; set; }
+    }
+}
---
+++ TwentyOne/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwentyOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Deck deck = new Deck();
+            deck = Shuffle(deck); //Used to call the function Shuffle below
+
+            foreach (Card card in deck.Cards)
+            {
+                Console.WriteLine(card.Face + " of " + card.Suit);
+            }
+            Console.WriteLine(deck.Cards.Count);
+            //deck.Cards = new List<Card>();
+
+
+            //An object of date type "Card" assigned to the variable cardOne
+            //Card cardOne = new Card();
+            //Assign the string "Queen" to the property "Face" of the object "cardOne"/////Used in place of a constructor
+            //cardOne.Face = "Queen";
+            //cardOne.Suit = "Spades";
+
+            //deck.Cards.Add(cardOne);
+
+
+
+            //Console.WriteLine(cardOne.Face + " of " + cardOne.Suit);
+            Console.ReadLine();
+        }
+
+        public static Deck Shuffle(Deck deck) //Function in C#, called above using "deck = Shuffle(deck);"
+        {
+            List<Card> TempList = new List<Card>();
+            Random random = new Random();
+
+            while (deck.Cards.Count > 0)
+            {
+                int randomIndex = random.Next(0, deck.Cards.Count);
+                TempList.Add(deck.Cards[randomIndex]);
+                deck.Cards.RemoveAt(randomIndex);
+            }
+            deck.Cards = TempList;
+            return deck;
+        }
+    }
+}
