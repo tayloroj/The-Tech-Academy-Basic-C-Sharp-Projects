00900520c57846c735ad4f84c0f3c592804d567d
Andy Taylor <andrew.taylor_22@yahoo.com>
2024-05-10 11:41:30 +0100

    Calling Methods Assignment

---
+++ CallingMethods/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethods
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ask the user to enter a number they want to do the math operations on
+            //Call each method in turn, passing the user input to the method. Display the returned integer to the screen
+            Console.WriteLine("Enter the amount of your monthly vehicle payment in whole numbers to find our how much interest you pay each month: ");
+            int vehiclePymt = Convert.ToInt32(Console.ReadLine());
+            int vehicleTax = TaxMethods.Vehicle(vehiclePymt);
+            Console.WriteLine("You have paid $" + vehicleTax + " in tax this month.");
+        
+            Console.WriteLine("\nEnter the amount of your monthly house payment in whole numbers to find our how much interest you pay each month: ");
+            int housePymt = Convert.ToInt32(Console.ReadLine());
+            int propertyTax = TaxMethods.House(housePymt);
+            Console.WriteLine("You have paid $" + propertyTax + " in tax this month.");
+
+            Console.WriteLine("\nEnter the amount of your monthly loan payment in whole numbers to find our how much interest you pay each month: ");
+            int loanPymt = Convert.ToInt32(Console.ReadLine());
+            int Tax = TaxMethods.Loan(loanPymt);
+            Console.WriteLine("You have paid $" + Tax + " in tax this month.");
+
+            Console.WriteLine("\nThe total amount of taxes you have paid this month is: $" + (vehicleTax + propertyTax + Tax));
+            Console.ReadLine();
+        }
+
+    }
+}
---
+++ CallingMethods/TaxMethods.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethods
+{
+    public class TaxMethods
+    {
+        //Three methods that take one integer parameter and return an integer
+        public static int Vehicle(int vehiclePymt)
+        {
+            double vehicleTax = vehiclePymt * .05;
+            return (int)vehicleTax;
+        }
+
+        public static int House(int housePymt)
+        {
+            double propertyTax = housePymt * .08;
+            return (int)propertyTax;
+        }
+
+        public static int Loan(int loanPymt)
+        {
+            double Tax = loanPymt * .18;
+            return (int)Tax;
+        }        
+    }
+}
