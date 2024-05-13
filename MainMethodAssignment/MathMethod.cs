4a0c84dc77cbbdba800b1604f0fc351fcb2cb042
Andy Taylor <andrew.taylor_22@yahoo.com>
2024-05-13 11:04:39 +0100

    Main Method Assignment

---
+++ MainMethodAssignment/MathMethod.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodAssignment
+{
+    public class MathMethod
+    {
+        //First method that takes an integer
+        public int MathOp(int number)
+        { 
+            return number + 22; 
+        }
+        //Second method that takes a decimal
+        public int MathOp(decimal number) 
+        {
+            //Convert the decimal to an int to perform the math operation
+            int newNum = Convert.ToInt32(number);
+            return newNum * 15;
+        }
+        //Third MathOp method that takes a string
+        public int MathOp(string number) 
+        {
+            //Convert the string to an int to perform the math operation
+            int newNum = Convert.ToInt32(number);
+            return newNum % 6;
+        }
+    }
+}
---
+++ MainMethodAssignment/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Instantiate the MathMethod class
+            MathMethod math1 = new MathMethod();
+            //Call the first method with an int parameter
+            Console.WriteLine(math1.MathOp(9));
+            //Call the second method with a decimal parameter
+            Console.WriteLine(math1.MathOp(9.5m));
+            //Call the third method with a string parameter
+            Console.Write(math1.MathOp("9"));
+            Console.ReadLine();
+        }
+    }
+}
