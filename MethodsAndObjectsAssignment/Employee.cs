420381837474e53de7e828f8b2fd5268af354048
Andy Taylor <andrew.taylor_22@yahoo.com>
2024-06-10 15:12:50 +0100

    Methods and Objects Assignment

---
+++ MethodsAndObjectsAssignment/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsAndObjectsAssignment
+{
+    //Define the Employee class that inherits from Person
+    public class Employee : Person
+    {
+        //Property to store the employee ID
+        public int Id { get; set; }
+    }
+}
---
+++ MethodsAndObjectsAssignment/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsAndObjectsAssignment
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //Instantiate and initalize an Employee object/Set the first name and set the last name
+            Employee employee = new Employee() { FirstName = "Sample", LastName = "Student" };
+            //Call the SayName method on the EMployee object
+            employee.SayName();
+            Console.ReadLine();
+        }
+    }
+}
