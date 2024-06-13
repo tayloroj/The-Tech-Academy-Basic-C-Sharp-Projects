9e5a33f4da522cbac9ab1a1fb8143dce549385e7
Andy Taylor <andrew.taylor_22@yahoo.com>
2024-06-13 15:40:45 +0100

    Practice with Inheritance, Polymorphism, Abstract Classes and Interfaces

--- TwentyOne/Program.cs
+++ TwentyOne/Program.cs
@@ -10,15 +10,15 @@ namespace TwentyOne
     {
         static void Main(string[] args)
         {
-            deck deck = new deck();
-            deck.shuffle(3);
+            Deck deck = new Deck();
+            deck.Shuffle(3);
 
-            foreach (card card in deck.cards)
+            foreach (Card card in deck.Cards)
             {
-                console.writeline(card.face + " of " + card.suit);
+                Console.WriteLine(card.Face + " of " + card.Suit);
             }
-            console.writeline(deck.cards.count);
-            console.readline();
+            Console.WriteLine(deck.Cards.Count);
+            Console.ReadLine();
         }        
     }
 }
--- TwentyOne/TwentyOneGame.cs
+++ TwentyOne/TwentyOneGame.cs
@@ -6,9 +6,18 @@ using System.Threading.Tasks;
 
 namespace TwentyOne
 {
-    public class TwentyOneGame : Game
+    public class TwentyOneGame : Game, IWalkAway
     {
-        public void Play()
+        public override void Play()
+        {
+            throw new NotImplementedException();
+        }
+        public override void ListPlayers()
+        {
+            Console.WriteLine("21 Players:");
+            base.ListPlayers();
+        }
+        public void WalkAway(Player player)
         {
             throw new NotImplementedException();
         }
