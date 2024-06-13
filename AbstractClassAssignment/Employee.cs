61853704e6794ddc35c1d9e022f5661a5148e846
Andy Taylor <andrew.taylor_22@yahoo.com>
2024-06-13 15:27:11 +0100

    Abstract Class Assignment

---
+++ AbstractClassAssignment/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClassAssignment
+{
+    public class Employee : Person
+    {
+        public int Id { get; set; }
+        // Implement the SayName method inside the Employee class
+        public override void SayName()
+        {
+            // Print the full name of the employee
+            Console.WriteLine("Name: " + firstName + " " + lastName);
+        }
+    }
+}
