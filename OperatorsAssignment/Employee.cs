9b7b9015a3c377c8759d45f966a8ce1a62ad3d20
Andy Taylor <andrew.taylor_22@yahoo.com>
2024-06-17 14:02:37 +0100

    Operators Assignment

---
+++ OperatorsAssignment/Employee.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorsAssignment
+{
+    public class Employee
+    {
+        //Properties for the Employee class
+        public int Id { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+
+        //Overloading the "==" operator
+        public static bool operator ==(Employee employee1, Employee employee2)
+        {
+            //If both are null, or both are the same instancem return true
+            if (ReferenceEquals(employee1, employee2)) return true;
+
+            // If one is null, but not both, return false
+            if (((object)employee1 == null) || ((object)employee2 == null)) return false;
+
+            //Return true if the fields match
+            return employee1.Id == employee2.Id;
+        }
+        //Overloading the "!=" operator
+        public static bool operator !=(Employee employee1, Employee employee2)
+        //Return the opposite of the "==" operator
+        { return !(employee1 == employee2); }
+        //Override Equals method for the "==" operator to function correctly
+        public override bool Equals(object obj)
+        {
+            //Check if the provided object is an Employee
+            if (obj is Employee)
+            {
+                //Cast the object to Employee and compare Ids
+                return this.Id == ((Employee)obj).Id;
+            }
+            //If not an Employee, return false
+            return false;
+        }
+        // Override GetHashCode method
+        public override int GetHashCode()
+        {
+            // Return the Id as the hash code
+            return this.Id.GetHashCode();
+        }
+    }
+}
