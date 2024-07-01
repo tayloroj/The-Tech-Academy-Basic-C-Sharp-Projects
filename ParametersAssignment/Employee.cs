579532805c04e08dea3a3630169c925c96f2ae73
Andy Taylor <andrew.taylor_22@yahoo.com>
2024-07-01 09:14:04 +0100

    Parameters Assignment

---
+++ ParametersAssignment/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParametersAssignment
+{
+    // Define the Employee class with a generic type parameter T
+    public class Employee<T>
+    {
+        // Property to store a list of items of type T
+        public List<T> Things { get; set; }
+
+        // Constructor to initialize the Things property
+        public Employee()
+        {
+            Things = new List<T>();
+        }
+    }
+}
