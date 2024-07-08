71a47156f8913dbb4c84dc397b9c5149ce85e92f
Andy Taylor <andrew.taylor_22@yahoo.com>
2024-07-08 14:33:09 +0100

    TwentyOne Enums update

--- TwentyOne/Card.cs
+++ TwentyOne/Card.cs
@@ -7,13 +7,33 @@ using System.Threading.Tasks;
 namespace TwentyOne
 {
     public class Card
+    {      
+        public Suit Suit { get; set; }
+        public Face Face { get; set; }
+        
+    }
+    public enum Suit
     {
-        public Card() 
-        {
-            Suit = "Spades";
-            Face = "Two";
-        }
-        public string Suit { get; set; }
-        public string Face { get; set; }
+        Clubs,
+        Diamonds,
+        Hearts,
+        Spades
+    }
+
+    public enum Face
+    {
+        Two,
+        Three,
+        Four,
+        Five, 
+        Six,
+        Seven,
+        Eight,
+        Nine,
+        Ten,
+        Jack,
+        Queen,
+        King,
+        Ace
     }
 }
