8c5fff22e07fd80577293a5d96ace2f26c67676c
Andy Taylor <andrew.taylor_22@yahoo.com>
2024-07-09 10:20:22 +0100

    Struct Assignment

---
+++ StructAssignment/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructAssignment
+{
+    
+    //Define a struct called Number
+    public struct Number 
+    {
+        //Define a property named Amount of type decimal
+        public decimal Amount { get; set; }
+    }
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create an instance of the Number struct
+            Number number = new Number();
+            
+            //Assign a value to the Amount property of the Number instance
+            number.Amount = 129.22m;
+
+            //Print the value of the Amount property to the console
+            Console.WriteLine("The amount is: " + number.Amount);
+
+            //Pause the console to view the result
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadLine();
+        }
+    }
+}
