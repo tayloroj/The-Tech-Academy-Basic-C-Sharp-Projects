c982d0f1736a2c6faec0b72c1d560b526124070c
Andy Taylor <andrew.taylor_22@yahoo.com>
2024-07-18 15:54:23 +0100

    Constructor Assignment

---
+++ ConstructorAssignment/Car.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorAssignment
+{
+    class Car
+    {
+        // Constant variable for maximum speed
+        private const int MaxSpeed = 200;
+
+        // Auto-implemented properties for Make and Model
+        public string Make { get; private set; }
+        public string Model { get; private set; }
+
+        // Constructor that takes both make and model as parameters
+        public Car(string make, string model) : this(make) // Chaining to the constructor with one parameter
+        {
+            // Set the Model property
+            this.Model = model;
+        }
+
+        // Constructor that takes only make as a parameter
+        public Car(string make)
+        {
+            // Set the Make property
+            this.Make = make;
+        }
+
+        // Method to display car details
+        public void DisplayCarDetails()
+        {
+            // Using the 'var' keyword to declare a variable
+            var carDetails = $"Car Make: {Make}, Car Model: {Model}, Max Speed: {MaxSpeed} km/h";
+
+            // Print the car details to the console
+            Console.WriteLine(carDetails);
+        }
+    }
+}
---
+++ ConstructorAssignment/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorAssignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create an instance of the Car class using the constructor with two parameters
+            Car myCar = new Car("Toyota", "Corolla");
+
+            // Display the car's details
+            myCar.DisplayCarDetails();
+        }
+    }
+}
