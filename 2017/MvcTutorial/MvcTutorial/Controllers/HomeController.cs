ef89678d5519e67cb8e8eaa7d3e12f5d8cbb4880
Andy Taylor <andrew.taylor_22@yahoo.com>
2024-11-25 13:04:08 +0000

    Nov update 2024

---
+++ 2017/MvcTutorial/MvcTutorial/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using MvcTutorial.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcTutorial.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            User user = new User();
+            user.Id = 1;
+            user.FirstName = "Johnny";
+            user.LastName = "Williams";
+            user.Age = 35;
+            return View(user);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
