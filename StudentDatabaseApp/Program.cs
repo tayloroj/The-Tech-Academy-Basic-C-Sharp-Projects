10202f4a09f449381ca3be0f4b656acd00808dbe
Andy Taylor <andrew.taylor_22@yahoo.com>
2024-11-27 12:40:32 +0000

    NETFrameworkFinalAssignment

---
+++ StudentDatabaseApp/Program.cs
@@ -0,0 +1,40 @@
+﻿using StudentDatabaseApp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        using (var context = new StudentContext())
+        {
+            // Create a new student
+            var student = new Student
+            {
+                Name = "John Doe",
+                Age = 20
+            };
+
+            // Add the student to the context
+            context.Students.Add(student);
+
+            // Save changes to the database
+            context.SaveChanges();
+
+            Console.WriteLine("Student added successfully!");
+
+            // Query the database to verify the student was added
+            var addedStudent = context.Students.Find(1);
+            if (addedStudent != null)
+            {
+                Console.WriteLine($"Retrieved student: {addedStudent.Name}, Age: {addedStudent.Age}");
+            }
+        }
+
+        Console.WriteLine("Press any key to exit...");
+        Console.ReadKey();
+    }
+}
---
+++ StudentDatabaseApp/StudentContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StudentDatabaseApp;
+
+public class StudentContext : DbContext
+{
+    public StudentContext() : base("name=StudentDbConnection")
+    {
+    }
+
+    public DbSet<Student> Students { get; set; }
+}
